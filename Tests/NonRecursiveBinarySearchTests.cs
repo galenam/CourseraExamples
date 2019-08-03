8b3f73f8db62b10113c810b8ff37dd6e2d20bf1e
Melnik Galina <melnikgg@gmail.com>
2019-08-03 22:38:25 +0300

    more non recursive tests

--- Tests/NonRecursiveBinarySearchTests.cs
+++ Tests/NonRecursiveBinarySearchTests.cs
@@ -10,6 +10,10 @@ namespace Tests
         {
 
             yield return (new int[] { }, 1, false);
+
+            yield return (new int[] { 5 }, 1, false);
+            yield return (new int[] { 5 }, 5, true);
+
             yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10 }, 11, false);
             yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10 }, -1, false);
             yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10 }, 2, false);
@@ -23,6 +27,21 @@ namespace Tests
             yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10 }, 7, true);
             yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10 }, 9, true);
             yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10 }, 10, true);
+
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 12, false);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, -1, false);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 2, false);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 8, false);
+
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 1, true);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 3, true);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 4, true);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 5, true);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 6, true);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 7, true);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 9, true);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 10, true);
+            yield return (new int[] { 1, 3, 4, 5, 6, 7, 9, 10, 11 }, 11, true);
         }
 
         [TestCaseSource(nameof(SourceTestSearch))]
